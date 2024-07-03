da4077b9a0f044502a626978b518febd72e63619
egor <ega.nik5674@gmail.com>
2024-07-03 10:11:22 +0300

    Init settings

---
+++ src/settings.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+EMPLOYEE_DATA_PATH = Path(__file__).parent.parent.joinpath("data", "employees.json")
+
+TASKS_DATA_PATH = Path(__file__).parent.parent.joinpath("data", "tasks.json")
