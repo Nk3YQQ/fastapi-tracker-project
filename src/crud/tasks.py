25c5101d372c85c27969058ab5b5da64b3b99106
egor <ega.nik5674@gmail.com>
2024-07-05 18:08:56 +0300

    Update routers

---
+++ src/crud/tasks.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.crud.base import CRUD
+from src.database import get_db
+from src.models import Task
+
+
+class TaskCRUD(CRUD):
+    model = Task
+
+    def __init__(self, db: AsyncSession = Depends(get_db)):
+        super().__init__(db)
--- src/routers/employees.py
+++ src/routers/employees.py
@@ -1,47 +1,47 @@
-from fastapi import status, Depends
+from fastapi import status, Depends, APIRouter
 
-from src.routers.base import APIRouter
+from src.crud.employees import EmployeeCRUD
 from src.schemas import Employee, EmployeeCreate, EmployeePut, EmployeePatch
-from src.views.employees import EmployeeView
 
 router = APIRouter()
 
 
 @router.post("/", response_model=Employee, status_code=status.HTTP_201_CREATED)
-async def create_employee(response_data: EmployeeCreate, view: EmployeeView = Depends()):
-    created_employee = await view.create(response_data)
+async def create_employee(response_data: EmployeeCreate, crud: EmployeeCRUD = Depends()):
+    created_employee = await crud.create_instance(response_data)
 
     return created_employee
 
 
 @router.get("/", response_model=list[Employee])
-async def read_employees(view: EmployeeView = Depends(), skip: int = 0, limit: int = 10):
-    employees_list = await view.read_all(skip, limit)
+async def read_employees(crud: EmployeeCRUD = Depends(), skip: int = 0, limit: int = 10):
+    employees_list = await crud.read_all_instances(skip, limit)
 
     return employees_list
 
 
 @router.get("/{employee_id}", response_model=Employee)
-async def read_employee(employee_id: int, view: EmployeeView = Depends()):
-    employee = await view.read_one(employee_id)
+async def read_employee(employee_id: int, crud: EmployeeCRUD = Depends()):
+    employee = await crud.read_one_instance(employee_id)
 
     return employee
 
 
 @router.put("/{employee_id}", response_model=Employee)
-async def update_employee(employee_id: int, response_data: EmployeePut, view: EmployeeView = Depends()):
-    employee = await view.put(employee_id, response_data)
+async def update_employee(employee_id: int, response_data: EmployeePut, crud: EmployeeCRUD = Depends()):
+    employee = await crud.update_instance(employee_id, response_data)
 
     return employee
 
 
 @router.patch("/{employee_id}", response_model=Employee)
-async def update_employee_particular(employee_id: int, response_data: EmployeePatch, view: EmployeeView = Depends()):
-    employee = await view.patch(employee_id, response_data)
+async def update_employee_particular(employee_id: int, response_data: EmployeePatch, crud: EmployeeCRUD = Depends()):
+
+    employee = await crud.update_instance(employee_id, response_data)
 
     return employee
 
 
 @router.delete("/{employee_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_employee(employee_id: int, view: EmployeeView = Depends()):
-    await view.delete(employee_id)
+async def delete_employee(employee_id: int, crud: EmployeeCRUD = Depends()):
+    await crud.delete_instance(employee_id)
