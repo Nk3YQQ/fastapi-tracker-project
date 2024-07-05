ba65aac52d670a3fb2ac078c8cf502c09bc4ce2c
egor <ega.nik5674@gmail.com>
2024-07-05 18:09:35 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 from src.routers.employees import router as employees_router
 from src.routers.tasks import router as tasks_router
+from src.routers.employees_tasks import router as employees_tasks_router
 
 app = FastAPI(title='Tracker App')
 
 
 app.include_router(employees_router, prefix='/employees', tags=['employees'])
 app.include_router(tasks_router, prefix='/tasks', tags=['tasks'])
+app.include_router(employees_tasks_router, prefix='/employees/tasks', tags=['employees_tasks'])
